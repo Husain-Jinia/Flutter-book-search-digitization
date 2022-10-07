96b2967c3d912d5a8fda2ab76fdb4dd347089291
husain-teachedison <husain@teachedison.com>
2022-10-07 22:59:33 +0530

    Setting up stepper in homepage

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,8 +8,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home page"),
+      ),
+      body: Stepper(
+        type:StepperType.horizontal,
+        currentStep: _index,
+        onStepCancel: () {
+          if (_index > 0) {
+            setState(() {
+              _index -= 1;
+            });
+          }
+        },
+        onStepContinue: () {
+          if (_index <= 0) {
+            setState(() {
+              _index += 1;
+            });
+          }
+        },
+        onStepTapped: (int index) {
+          setState(() {
+            _index = index;
+          });
+        },
+        steps: <Step>[
+          Step(
+            title: const Text('Image'),
+            content: Container(
+                alignment: Alignment.centerLeft,
+                child: const Text('Image')),
+          ),
+          Step(
+            title: const Text('Search query'),
+            content: Center(
+              child: TextFormField( 
+                decoration: InputDecoration(
+                  label: const Text("Search query"),
+                  hintText: "Enter the name of the book you want to find.",
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                ),
+
+              ),
+            ),
+          ),
+        ],
+      )
+    );
   }
 }
