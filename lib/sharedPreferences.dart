7f808c76840095544d85b427aaf4a10dcdb3557b
Husain-Jinia <husainjiniaga@gmail.com>
2022-10-12 13:06:43 +0530

    Added shared preference for data persistence in local storage

---
+++ lib/sharedPreferences.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesService {
+  static late SharedPreferences prefs;
+
+  Future<bool> saveToSharedPref(String key, String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(key, token);
+  }
+
+  Future<String?> getFromSharedPref(String key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+}
